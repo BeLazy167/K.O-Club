4ff4039ee9b964f7b7d5958ef898ae0376ed70cb
belazy167 <dhruvkhara167@gmail.com>
2024-04-22 23:29:43 -0400

    added loader and basic ui fixes

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,8 +1,27 @@
+import { Skeleton } from "~/components/ui/skeleton";
+
 export default function Loading() {
   return (
-    <div className="loading">
-      <div className="loading__spinner" />
-      This is suspense loading
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div className="flex flex-col space-y-6">
+        <div className="text-center text-2xl font-bold">
+          Hold your horses! Your epic fight is loading...
+        </div>
+        <div className="mt-4 text-center text-lg">
+          Don't worry, we're just warming up the punching bags and polishing the
+          boxing gloves!
+        </div>
+        <div className="mt-2 text-center text-lg">
+          In the meantime, feel free to practice your victory dance!
+        </div>
+      </div>
+      <Skeleton className="h-[200px] w-[400px] rounded-xl mt-2" />
+      <div className="space-y-4">
+        <Skeleton className="h-6 w-[400px]" />
+        <Skeleton className="h-6 w-[350px]" />
+        <Skeleton className="h-6 w-[300px]" />
+        <Skeleton className="h-6 w-[250px]" />
+      </div>
     </div>
   );
 }
