01eb9510be0f9decefd8233e33804d8198dc8a00
belazy167 <dhruvkhara167@gmail.com>
2024-04-30 21:31:14 -0400

    bug fix

--- client/src/app/api/allFights/route.ts
+++ client/src/app/api/allFights/route.ts
@@ -1,65 +1,65 @@
-// Route : /api/allFights
+// // Route : /api/allFights
 
-import { NextResponse } from "next/server";
-import { and, desc, eq } from "drizzle-orm/expressions";
-import { getServerSession } from "next-auth";
-import { db } from "~/server/db";
-import { authOptions } from "~/server/auth";
-import { fights, fightsRelations, users } from "~/server/db/schema";
-import { alias } from "drizzle-orm/pg-core";
-import { unstable_noStore as no_store } from "next/cache";
+// import { NextResponse } from "next/server";
+// import { and, desc, eq } from "drizzle-orm/expressions";
+// import { getServerSession } from "next-auth";
+// import { db } from "~/server/db";
+// import { authOptions } from "~/server/auth";
+// import { fights, fightsRelations, users } from "~/server/db/schema";
+// import { alias } from "drizzle-orm/pg-core";
+// import { unstable_noStore as no_store } from "next/cache";
 
-export async function GET(request: Request) {
-  no_store(); // Disable caching for this request
+// export async function GET(request: Request) {
+//   no_store(); // Disable caching for this request
 
-  try {
-    const author = alias(users, "author");
-    const challenged = alias(users, "challenged");
+//   try {
+//     const author = alias(users, "author");
+//     const challenged = alias(users, "challenged");
 
-    // Fetch accepted fights from the database
-    const acceptedFights = await db
-      .select({
-        id: fights.id,
-        title: fights.title,
-        description: fights.description,
-        location: fights.location,
-        dateTime: fights.dateTime,
-        createdAt: fights.createdAt,
-        author: {
-          id: author.id,
-          name: author.name,
-          username: author.username,
-          image: author.image,
-          email: author.email,
-        },
-        challenged: {
-          id: challenged.id,
-          name: challenged.name,
-          username: challenged.username,
-          image: challenged.image,
-          email: challenged.email,
-        },
-      })
-      .from(fights)
-      .leftJoin(author, eq(fights.authorId, author.id))
-      .leftJoin(challenged, eq(fights.challengedId, challenged.id))
-      .where(
-        and(
-          eq(fights.challengedAccepted, true),
-          eq(fights.authorAccepted, true),
-        ),
-      )
-      .orderBy(desc(fights.createdAt));
+//     // Fetch accepted fights from the database
+//     const acceptedFights = await db
+//       .select({
+//         id: fights.id,
+//         title: fights.title,
+//         description: fights.description,
+//         location: fights.location,
+//         dateTime: fights.dateTime,
+//         createdAt: fights.createdAt,
+//         author: {
+//           id: author.id,
+//           name: author.name,
+//           username: author.username,
+//           image: author.image,
+//           email: author.email,
+//         },
+//         challenged: {
+//           id: challenged.id,
+//           name: challenged.name,
+//           username: challenged.username,
+//           image: challenged.image,
+//           email: challenged.email,
+//         },
+//       })
+//       .from(fights)
+//       .leftJoin(author, eq(fights.authorId, author.id))
+//       .leftJoin(challenged, eq(fights.challengedId, challenged.id))
+//       .where(
+//         and(
+//           eq(fights.challengedAccepted, true),
+//           eq(fights.authorAccepted, true),
+//         ),
+//       )
+//       .orderBy(desc(fights.createdAt));
 
-    // Return the accepted fights as a JSON response with status code 200
-    return NextResponse.json(acceptedFights, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching accepted fights:", error);
+//     // Return the accepted fights as a JSON response with status code 200
+//     return NextResponse.json(acceptedFights, { status: 200 });
+//   } catch (error) {
+//     console.error("Error fetching accepted fights:", error);
 
-    // Return an error message as a JSON response with status code 500
-    return NextResponse.json(
-      { message: "Failed to fetch accepted fights" },
-      { status: 500 },
-    );
-  }
-}
+//     // Return an error message as a JSON response with status code 500
+//     return NextResponse.json(
+//       { message: "Failed to fetch accepted fights" },
+//       { status: 500 },
+//     );
+//   }
+// }
